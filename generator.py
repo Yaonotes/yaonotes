c2f30af3b4379cfd238d2b4f775c413b5eeb5419
Xiaozhe Yao <yaoxiaozhe@email.szu.edu.cn>
2019-11-26 17:04:20 +0100

    re-enable history generator

--- generator.py
+++ generator.py
@@ -159,7 +159,7 @@ def parse():
         else:
             create_folder(os.path.join("_site", each[5:]))
             iterate_folders(each)
-    # generate_history()
+    generate_history()
 
 if __name__ == "__main__":
     prepare()
